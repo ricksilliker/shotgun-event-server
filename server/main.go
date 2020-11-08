61697ad6c173396c52e55a17f52fdc259be9ce8c
Rick Silliker <ricksilliker@gmail.com>
2020-11-07 23:12:36 -0600

    add event processing

--- server/main.go
+++ server/main.go
@@ -1,11 +1,47 @@
 package server
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	shotgun_consumer "github.com/ricksilliker/shotgun-event-server/shotgun-consumer"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+)
 
-func processEvents(config ServerConfig) {
+func processEvents(config *ServerConfig, lastEventID int) {
+	//var plugins []string
+	var continueToProcess bool
 
+	url := "https://brazenanimation.shotgunstudio.com/api/v1"
+	auth := shotgun_consumer.AuthenticateAsScript(url, config.ShotgunScriptName, config.ShotgunScriptKey)
+
+	for ok := true; ok; ok = !continueToProcess {
+		events, _ := shotgun_consumer.GetNewEvents(url, auth, lastEventID)
+		for event := range events {
+			//for _, plugin := range plugins {
+			//	plugin.process(event)
+			//}
+			logrus.WithField("eventId", event.ID).Info("Processing event")
+			storeEventID(event)
+			lastEventID = event.ID
+		}
+	}
+}
+
+func storeEventID(event *shotgun_consumer.EventLogEntry) {
+	data, err := json.Marshal(event)
+	if err != nil {
+		logrus.Error("failed to marshal ShotgunEventEntry data")
+	}
+	err = ioutil.WriteFile("lastProcessedEvent.txt", data, 0775)
+	if err != nil {
+		logrus.Error("failed to write out ShotgunEventEntry information")
+	}
 }
 
 func main() {
 	fmt.Println("Hello World")
+	config, _ := NewServerConfigFromFile("server.json")
+	var lastEventID = 3712449
+	processEvents(config, lastEventID)
 }
--- shotgun-consumer/main.go
+++ shotgun-consumer/main.go
@@ -1,11 +1,14 @@
 package shotgun_consumer
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
@@ -28,6 +31,35 @@ type ShotgunErrorBody struct {
 	Detail string `json:"detail"`
 	Source map[string]interface{} `json:"source"`
 	Meta string `json:"meta"`
+}
+
+type PaginationParameter struct {
+	Size int `json:"size,omitempty"`
+	Number int `json:"number,omitempty"`
+}
+
+type SearchRequest struct {
+	Filters [][]string            	`json:"filters"`
+	Fields  []string              	`json:"fields"`
+	Sort    string              	`json:"sort,omitempty"`
+	Page  	*PaginationParameter 	`json:"page,omitempty"`
+}
+
+type SearchResponse struct {
+	Data []Record `json:"data"`
+	PageLinks struct {
+		CurrentPage string `json:"self"`
+		NextPage string `json:"next"`
+		PreviousPage string `json:"prev"`
+	} `json:"links"`
+}
+
+type Record struct {
+	ID int `json:"id"`
+}
+
+type EventLogEntry struct {
+	ID int `json:"id"`
 }
 
 
@@ -77,4 +109,91 @@ func AuthenticateAsScript(shotgunURL, scriptName, scriptKey string) *ShotgunAuth
 	}
 
 	return &a
+}
+
+
+func GetLatestEventLogEntry(shotgunURL string, authToken *ShotgunAuth) (*EventLogEntry, error) {
+	searchURL := shotgunURL + "/entity/event_log_entry/_search"
+	reqBody := SearchRequest{
+		Filters: [][]string {},
+		Fields:[]string {
+			"id",
+		},
+		Sort: "-created_at",
+		Page: &PaginationParameter {
+			Size: 10,
+			Number: 1,
+		},
+	}
+	jsonBody, _ := json.Marshal(reqBody)
+	bodyBytes := bytes.NewBuffer(jsonBody)
+	req, _ := http.NewRequest("POST", searchURL, bodyBytes)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/vnd+shotgun.api3_array+json")
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", authToken.TokenType, authToken.AccessToken))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Error("failed to query Shotgun EventLogEntries")
+		return nil, err
+	}
+	respBytes, _ := ioutil.ReadAll(resp.Body)
+	searchResp := SearchResponse{}
+	err = json.Unmarshal(respBytes, &searchResp)
+	if err != nil {
+		logrus.Error("failed to unmarshal Shotgun response")
+		return nil, err
+	}
+
+	return &EventLogEntry{ searchResp.Data[0].ID }, nil
+}
+
+func GetNewEvents(shotgunURL string, authToken *ShotgunAuth, lastEventID int) (<-chan *EventLogEntry, error){
+	searchURL := shotgunURL + "/entity/event_log_entry/_search"
+	reqBody := SearchRequest{
+		Filters: [][]string {
+			{
+				"id", "greater_than", strconv.Itoa(lastEventID),
+			},
+		},
+		Fields:[]string {
+			"id",
+		},
+		Sort: "id",
+		Page: &PaginationParameter {
+			Size: 10,
+			Number: 1,
+		},
+	}
+	jsonBody, _ := json.Marshal(reqBody)
+	bodyBytes := bytes.NewBuffer(jsonBody)
+	req, _ := http.NewRequest("POST", searchURL, bodyBytes)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/vnd+shotgun.api3_array+json")
+	req.Header.Add("Authorization", fmt.Sprintf("%s %s", authToken.TokenType, authToken.AccessToken))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Error("failed to query Shotgun EventLogEntries")
+		return nil, err
+	}
+	respBytes, _ := ioutil.ReadAll(resp.Body)
+	searchResp := SearchResponse{}
+	err = json.Unmarshal(respBytes, &searchResp)
+	if err != nil {
+		logrus.Error("failed to unmarshal Shotgun response")
+		return nil, err
+	}
+
+	chnl := make(chan *EventLogEntry)
+	go func() {
+		for _, entry := range searchResp.Data {
+			chnl <- &EventLogEntry{ entry.ID }
+		}
+		close(chnl)
+	}()
+
+	return chnl, nil
 }
